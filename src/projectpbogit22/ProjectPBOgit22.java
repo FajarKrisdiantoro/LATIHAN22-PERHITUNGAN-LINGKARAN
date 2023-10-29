4a823dad003ca2550f289793cf78f8c6957d46a4
LENOVO <LENOVO@DESKTOP-VL4046V>
2023-10-29 21:13:06 +0700

    Fist commit PROGRAM PERHITUNGAN LINGKARAN

---
+++ src/projectpbogit22/ProjectPBOgit22.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package projectpbogit22;
+import java.util.Scanner;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class ProjectPBOgit22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        double diameter = 0;
+        double radius;
+        double area;
+        double circumference;
+        boolean validInput = false;
+
+        while (!validInput) {
+            System.out.print("Masukkan nilai diameter lingkaran: ");
+            if (input.hasNextDouble()) {
+                diameter = input.nextDouble();
+                validInput = true;
+            } else {
+                System.out.println("Masukan tidak valid. Mohon masukkan angka.");
+                input.next(); // Menghapus input yang tidak valid
+            }
+        }
+
+        // Menghitung jari-jari, luas, dan keliling lingkaran
+        radius = diameter / 2;
+        area = Math.PI * Math.pow(radius, 2);
+        circumference = 2 * Math.PI * radius;
+
+        System.out.println("Jari-jari lingkaran: " + radius);
+        System.out.println("Luas lingkaran: " + area);
+        System.out.println("Keliling lingkaran: " + circumference);
+
+        input.close();
+    }
+    
+}
