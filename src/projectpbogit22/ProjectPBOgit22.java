b02dee68dbf7ea5fafa1a46a638ae34fd7693826
LENOVO <LENOVO@DESKTOP-VL4046V>
2023-10-30 22:24:25 +0700

    SECOND commit PROGRAM PERHITUNGAN LINGKARAN Menambahkan identitas

--- src/projectpbogit22/ProjectPBOgit22.java
+++ src/projectpbogit22/ProjectPBOgit22.java
@@ -2,6 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+/*
+NAMA : FAJAR KRISDIANTORO
+NIM  : 22205004
+PRODI: TI REG SORE
+*/
 package projectpbogit22;
 import java.util.Scanner;
 
